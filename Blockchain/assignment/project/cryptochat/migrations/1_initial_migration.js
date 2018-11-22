8ac1d2773c5668de61688a12f2703b99d6e43f6e
Miguel Chan <vvchan@outlook.com>
2018-11-23 01:05:50 +0800

    Add report

---
+++ Blockchain/assignment/project/cryptochat/migrations/1_initial_migration.js
@@ -0,0 +1,7 @@
+//var Migrations = artifacts.require("./Migrations.sol");
+const CryptoChat = artifacts.require("./CryptoChat.sol");
+
+module.exports = function(deployer) {
+  //deployer.deploy(Migrations);
+  deployer.deploy(CryptoChat);
+};
