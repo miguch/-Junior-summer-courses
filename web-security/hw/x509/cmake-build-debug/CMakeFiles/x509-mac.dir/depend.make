ac9e58c76b719b1ca534173d0065eb3f3ea5bcae
Miguel Chan <vvchan@outlook.com>
2018-12-06 10:32:17 +0800

    update

---
+++ web-security/hw/x509/cmake-build-debug/CMakeFiles/x509-mac.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for x509-mac.
+# This may be replaced when dependencies are built.
---
+++ web-security/hw/x509/cmake-build-debug/CMakeFiles/x509.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/x509.dir/main.cpp.o"
+  "x509.pdb"
+  "x509"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/x509.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
